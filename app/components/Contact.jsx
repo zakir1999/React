9b50d58dfec317674daf332d479c06ebc97014d7
zakir1999 <m.zakirhossen99@gmail.com>
2025-09-27 17:12:35 +0600

    Get in touch page create

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -6,12 +6,19 @@ export const Contact = () => {
   id="contact"
   className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
 >
-  <h4 className="text-center mb-2 text-lg font-Ovo">My portfolio</h4>
-  <h2 className="text-center text-5xl font-Ovo">My latest work</h2>
+  <h4 className="text-center mb-2 text-lg font-Ovo">Connect with me</h4>
+  <h2 className="text-center text-5xl font-Ovo">Get in touch</h2>
   <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
-    Welcome to my web development portfolio! Explore a collection of
-    projects showcasing my expertise in front-end development.
+   I'd love to hear from you! If you have any questions,comments, or feedback, please use the form below.
   </p>
+  <form className='max-w-2xl mx-auto'>
+    <div className='flex gap-6 mt-10 mb-8'>
+      <input type="text" placeholder='Enter your name' required  className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
+      <input type="text" placeholder='Enter your Email' required  className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
+    </div>
+    <textarea rows='6' placeholder='Enter your message' required  className='w-full p-4 mb-6 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'></textarea>
+    <button type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>Submit</button>
+  </form>
 </div>
 
   )
